02eaf4708f1d2ca632ab0c18828a37acd2760aa9
Thomas Auzard <48713682+noougatine@users.noreply.github.com>
2020-02-13 22:21:28 +0100

    Update cross_val.py

--- cross_val.py
+++ cross_val.py
@@ -4,16 +4,16 @@
 # In[ ]:
 
 
-def k_cross_val(pena, selection = False, features = None, normalized = 'standard') :
+def k_cross_val(pena, selection = False, features = None) :
     """
     5 fold cross validation.
     """
     cphs = []
     scores = []
-    x1, x2, y1, y2 = get_data_set(x, y, 1/5, selection, features, normalized = 'standard')
-    x2, x3, y2, y3 = get_data_set(x2, y2, 1/4, selection, features, normalized = 'standard')
-    x3, x4, y3, y4 = get_data_set(x3, y3, 1/3, selection, features, normalized = 'standard')
-    x4, x5, y4, y5 = get_data_set(x4, y4, 1/2, selection, features, normalized = 'standard')
+    x1, x2, y1, y2 = get_data_set(x, y, 1/5, selection, features)
+    x2, x3, y2, y3 = get_data_set(x2, y2, 1/4, selection, features)
+    x3, x4, y3, y4 = get_data_set(x3, y3, 1/3, selection, features)
+    x4, x5, y4, y5 = get_data_set(x4, y4, 1/2, selection, features)
     
     xs, ys = [x1, x2, x3, x4, x5], [y1, y2, y3, y4, y5]
     
